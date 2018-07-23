62e3a123984b65dc62f39d131315f017993d1adb
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-07-23 16:11:25 -0400

    Agregadas referencias abastecimiento central

--- app/models/bodega_central.rb
+++ app/models/bodega_central.rb
@@ -2,4 +2,5 @@ class BodegaCentral < ApplicationRecord
   has_many :bodeguero_obras
   has_one :bodeguero_central
   has_many :inventario_centrals
+  has_many :abastecimiento_centrals
 end
--- app/models/proveedor.rb
+++ app/models/proveedor.rb
@@ -2,4 +2,5 @@ class Proveedor < ApplicationRecord
   belongs_to :bodeguero_central
   belongs_to :encargado_compra
   has_many :abastecimiento_obras
+  has_many :abastecimiento_centrals
 end
