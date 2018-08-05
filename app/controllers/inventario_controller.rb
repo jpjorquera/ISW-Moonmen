dc5567098fa4665d7a9cb2cf1f10cddd4cd2a3d8
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-08-04 23:35:47 -0400

    fixes bodega obra

--- app/controllers/inventario_controller.rb
+++ app/controllers/inventario_controller.rb
@@ -1,17 +1,18 @@
 class InventarioController < ApplicationController
   def ver
     #current_user. id puesto
-
+    @central = false
     if current_user.puesto == 2
+      @central = true
       bodeguero = BodegueroCentral.find_by user_id: current_user.id
       n_bodega = bodeguero.bodega_central_id
       @localizacion = BodegaCentral.find(n_bodega).localizacion
       @material_bodega = InventarioCentral.joins(:material).where(bodega_central_id: n_bodega)
     else
-      bodeguero = BodegueroObras.find_by user_id: current_user.id
+      bodeguero = BodegueroObra.find_by user_id: current_user.id
       n_bodega = bodeguero.bodega_obra_id
       @localizacion = BodegaObra.find(n_bodega).localizacion
-      @material_bodega = InventarioObra.joins(:material).where(bodega_central_id: n_bodega)
+      @material_bodega = InventarioObra.joins(:material).where(bodega_obra_id: n_bodega)
     end
   end
 end
