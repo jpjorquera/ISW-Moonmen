98202b7fa188fb6fcf5b4497ad208073129f5511
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-08-04 23:21:26 -0400

    agregada ubicacion y centrado

--- app/controllers/inventario_controller.rb
+++ app/controllers/inventario_controller.rb
@@ -5,10 +5,12 @@ class InventarioController < ApplicationController
     if current_user.puesto == 2
       bodeguero = BodegueroCentral.find_by user_id: current_user.id
       n_bodega = bodeguero.bodega_central_id
+      @localizacion = BodegaCentral.find(n_bodega).localizacion
       @material_bodega = InventarioCentral.joins(:material).where(bodega_central_id: n_bodega)
     else
       bodeguero = BodegueroObras.find_by user_id: current_user.id
       n_bodega = bodeguero.bodega_obra_id
+      @localizacion = BodegaObra.find(n_bodega).localizacion
       @material_bodega = InventarioObra.joins(:material).where(bodega_central_id: n_bodega)
     end
   end
