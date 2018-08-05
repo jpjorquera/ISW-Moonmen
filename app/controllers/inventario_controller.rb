781d170d664ce269ecda326bece04c2eac5866b9
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-08-04 22:28:13 -0400

    actualizada vista a materiales segun tipo de bodeguero

--- app/controllers/inventario_controller.rb
+++ app/controllers/inventario_controller.rb
@@ -1,5 +1,15 @@
 class InventarioController < ApplicationController
   def ver
-    @material_bodega = InventarioCentral.joins(:material)
+    #current_user. id puesto
+
+    if current_user.puesto == 2
+      bodeguero = BodegueroCentral.find_by user_id: current_user.id
+      n_bodega = bodeguero.bodega_central_id
+      @material_bodega = InventarioCentral.joins(:material).where(bodega_central_id: n_bodega)
+    else
+      bodeguero = BodegueroObras.find_by user_id: current_user.id
+      n_bodega = bodeguero.bodega_obra_id
+      @material_bodega = InventarioObra.joins(:material).where(bodega_central_id: n_bodega)
+    end
   end
 end
