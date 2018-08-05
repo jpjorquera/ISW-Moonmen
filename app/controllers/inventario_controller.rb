2c6243f5cf0aafe7115243ad8fc9ab517fd9cda9
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-08-04 23:47:39 -0400

    agregado caso personal obra

--- app/controllers/inventario_controller.rb
+++ app/controllers/inventario_controller.rb
@@ -2,6 +2,7 @@ class InventarioController < ApplicationController
   def ver
     #current_user. id puesto
     @central = false
+    n_bodega = 1
     if current_user.puesto == 2
       @central = true
       bodeguero = BodegueroCentral.find_by user_id: current_user.id
@@ -9,8 +10,13 @@ class InventarioController < ApplicationController
       @localizacion = BodegaCentral.find(n_bodega).localizacion
       @material_bodega = InventarioCentral.joins(:material).where(bodega_central_id: n_bodega)
     else
-      bodeguero = BodegueroObra.find_by user_id: current_user.id
-      n_bodega = bodeguero.bodega_obra_id
+      if current_user.puesto == 1
+        bodeguero = BodegueroObra.find_by user_id: current_user.id
+        n_bodega = bodeguero.bodega_obra_id
+      else
+        trabajador = PersonalObra.find_by user_id: current_user.id
+        n_bodega = trabajador.bodeguero_obra.bodega_obra_id
+      end
       @localizacion = BodegaObra.find(n_bodega).localizacion
       @material_bodega = InventarioObra.joins(:material).where(bodega_obra_id: n_bodega)
     end
