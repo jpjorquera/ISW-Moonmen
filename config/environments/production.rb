113bc70d3159da889d7ec5b97f8c319623d0503e
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-08-05 23:31:00 -0400

    agregados cambios a production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mails configs
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'moonmen.gpi@gmail.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  user_name:            'moonmen.gpi',
+  password:             'A_sdf54321',
+  authentication:       'plain',
+  enable_starttls_auto: true }
 end
