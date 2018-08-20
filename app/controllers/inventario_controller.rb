c9ab0ea72eb430822561fb345c42865a4fb24f7f
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2018-08-20 02:01:08 -0300

    avances en retirar materiales

--- app/controllers/inventario_controller.rb
+++ app/controllers/inventario_controller.rb
@@ -3,6 +3,7 @@ class InventarioController < ApplicationController
     #current_user. id puesto
     @central = false
     n_bodega = 1
+    # Bodeguero central
     if current_user.puesto == 2
       @central = true
       bodeguero = BodegueroCentral.find_by user_id: current_user.id
@@ -10,9 +11,11 @@ class InventarioController < ApplicationController
       @localizacion = BodegaCentral.find(n_bodega).localizacion
       @material_bodega = InventarioCentral.joins(:material).where(bodega_central_id: n_bodega)
     else
+      # Bodeguero obra
       if current_user.puesto == 1
         bodeguero = BodegueroObra.find_by user_id: current_user.id
         n_bodega = bodeguero.bodega_obra_id
+      # Personal obra
       else
         trabajador = PersonalObra.find_by user_id: current_user.id
         n_bodega = trabajador.bodeguero_obra.bodega_obra_id
@@ -49,5 +52,36 @@ class InventarioController < ApplicationController
     end
     redirect_to inventario_path
     
+  end
+
+  def sacar
+    @personal = PersonalObra.find_by user_id: current_user
+    n_bodega = @personal.bodeguero_obra.bodega_obra_id
+    #@mat = InventarioObra.joins(:material).where(bodega_obra_id: n_bodega)
+    @inv = InventarioObra.joins(:material).where(bodega_obra_id: n_bodega)
+    @mat = InventarioObra.new
+    #@mat = @personal.bodeguero_obra.bodega_obra.inventario_obras.build
+  end
+
+  def update_sacar
+    @personal = PersonalObra.find_by user_id: current_user
+    n_bodega = @personal.bodeguero_obra.bodega_obra_id
+    io = params[:inventario_obra]
+    actual = InventarioObra.find_by(bodega_obra_id: n_bodega, material_id: io[:material_id])
+    dif = actual[:stock_obra].to_i - io[:stock_obra].to_i
+    if dif < 0
+      flash[:danger] = "No hay suficientes materiales"
+      redirect_to :action => "ver"
+    elsif  dif == 0
+      actual.update(stock_obra: dif)
+      flash[:success] = "Material retirado correctamente"
+      actual.delete
+      redirect_to :action => "ver"
+    else
+      actual.update(stock_obra: dif)
+      flash[:success] = "Material retirado correctamente"
+      redirect_to :action => "ver"
+    end
+
   end
 end
