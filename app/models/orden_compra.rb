a7a17e8fa31eeac6fb2e507db10103223378e26a
Leonardo Astudillo <leonardo.astudillov@sansano.usm.cl>
2018-08-29 22:52:12 -0300

    Se hizo cambios al modelo

--- app/models/orden_compra.rb
+++ app/models/orden_compra.rb
@@ -3,5 +3,5 @@ class OrdenCompra < ApplicationRecord
   belongs_to :proveedor, dependent: :destroy
   belongs_to :encargado_compra, dependent: :destroy
   has_many :registro_materials, :dependent => :destroy
-  accepts_nested_attributes_for :registro_materials, :reject_if => lambda {|a| a[:nombre_item].blank? && a[:sku].blank? && a[:cantidad].blank? && a[:precio].blank?}, :allow_destroy => true
+  accepts_nested_attributes_for :registro_materials, :reject_if => lambda {|a| a[:nombre_item].blank?}, :allow_destroy => true
 end
